86a5ef7f988d07de1b04d756d963670da71de067
kucheryavenkovn <kucheryavenkovn@gmail.com>
2021-04-01 09:10:30 +0700

    Добавил скрипт для регистрации службы RAS (Remote Administation Server)

---
+++ RAS/register-ras.bat
@@ -0,0 +1,15 @@
+@echo off
+rem %1 - полный номер версии 1С:Предприятия
+rem set SrvUserName=.\USR1CV8_RAS
+set SrvUserName=%2%
+rem set SrvUserPwd="Pass123"
+set SrvUserPwd=%3%
+set CtrlPort=1540
+set AgentName=localhost
+set RASPort=1545
+set SrvcName="1C:Enterprise 8.3 Remote Server"
+set BinPath="\"C:\Program Files\1cv8\%1\bin\ras.exe\" cluster --service --port=%RASPort% %AgentName%:%CtrlPort%"
+set Desctiption="1C:Enterprise 8.3 Remote Server"
+sc stop %SrvcName%
+sc delete %SrvcName%
+sc create %SrvcName% binPath= %BinPath% start= auto obj= %SrvUserName% password= %SrvUserPwd% displayname= %Desctiption%ra
